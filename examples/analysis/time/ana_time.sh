ef724fcb7d4aaafc76a03f4ecf977df65e5e11a2
sunfj1998 <sunfj1998@sjtu.edu.com>
2023-09-27 10:29:11 +0800

    tiny change

--- examples/analysis/time/ana_time.sh
+++ examples/analysis/time/ana_time.sh
@@ -2,27 +2,31 @@
 
 source cal_para.sh
 WORKDIR="$PWD"
-datadir=$WORKDIR/../../run_cubic/
-#datadir=/dssg/home/acct-phyxxy/phyxxy/mywork/sun-hubbard/run_cubic
-#datadir="$PWD"
 echo $WORKDIR
 cd  $WORKDIR
-rm time.dat
+for code in ${codearray}; do
+for beta in ${betaarray}; do
 for u in ${uarray}; do
+for rv in ${varray}; do
 for plqu in ${plquarray}; do
 for nu in ${nuarray}; do
 for nflr in ${nflrarray}; do
-for beta in ${betaarray}; do
-for L  in ${Larray}; do
-       cd $datadir
-        maindir=${code}.b${beta}.dtau${dtau}.t${rt}.U${u}.plqU${plqu}.alpha${alpha}.theta${theta}.Nf${nflr}.nu${nu}.L${L}.proj${lprojplqu}${lproju}
-        #echo ${maindir}
+# Create a new data file for each code
+datafile="time_${code}.b${beta}.U${u}.plqU${plqu}.V${rv}.Nf${nflr}.dat"
+echo -n > $datafile
+rm $datafile
+  for L  in ${Larray}; do
+       cd $WORKDIR
+        maindir=${code}.b${beta}.U${u}.plqU${plqu}.V${rv}.Nf${nflr}.L${L}
+        echo ${maindir}
          if [ -f $maindir/dqmc.out ]; then
            cd $maindir
-           time=$(grep 'The time of update'  dqmc.out|awk -F: '{printf "%.2f\n", $2}')
-           time1=$(grep 'Total time spent'  dqmc.out|awk -F: '{printf "%.2f\n", $2}')
-           echo ${L} ${time} ${time1} >> $WORKDIR/time.dat
+           time=$(grep -E "The_time_of_.*_update"  dqmc.out|awk -F: '{printf "%.2f\n", $2}')
+           time1=$(grep 'The_time_of_sweep_in_total'  dqmc.out|awk -F: '{printf "%.2f\n", $2}')
+           echo ${L} ${time} ${time1} >> $WORKDIR/$datafile
          fi
+done
+done
 done
 done
 done
